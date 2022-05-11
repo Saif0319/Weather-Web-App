7d3e066884a20155dfb414c1485afe6e476cc83c
Saif0319 <gamersaif2@gmail.com>
2022-05-11 21:43:34 +0300

    Added an option to convert the temperature to fahrenheit

--- app.js
+++ app.js
@@ -44,24 +44,52 @@ const updateUI = (data) => {
     if (card.classList.contains("d-none")){
         card.classList.remove("d-none")
     }
+
+
+
+
+
+    const F = document.getElementById("flexSwitchCheckDefault");
+
+    F.addEventListener("change", (e) => {
+        e.preventDefault()
+        if(F.checked){
+            details.innerHTML = `
+            <div class="text-muted text-uppercase text-center details">
+            <h5 class="my-3 ">${cityDets.EnglishName}</h5>
+            <div class="my-3">${weather.WeatherText}</div>
+            <div class="display-4 my-4">
+                <span>${convert(weather.Temperature.Metric.Value)}</span>
+                <span style="margin: -15px">&deg;F</span>
+            </div>
+        </div>
+            `
+        } else {
+            details.innerHTML = `
+        <div class="text-muted text-uppercase text-center details">
+                <h5 class="my-3 ">${cityDets.EnglishName}</h5>
+                <div class="my-3">${weather.WeatherText}</div>
+                <div class="display-4 my-4">
+                    <span>${Math.round(weather.Temperature.Metric.Value)}</span>
+                    <span style="margin: -15px">&deg;C</span>
+                </div>
+            </div>`
+          }
+    })
+
+
+
 }
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+// Convert to F 
+const convert = cel => {
+    let c = Math.round(cel);
+    let converted = (c*1.8) + 32;
+    return Math.round(converted);
+}
 
 
 
@@ -82,6 +110,9 @@ cityForm.addEventListener("submit", e => {
     forecast.updateCity(city)
     .then(data => updateUI(data))
     .catch(error => console.log(error))
+
+    const F = document.getElementById("flexSwitchCheckDefault");
+    F.checked = false;
 
     // Set local storage
     localStorage.setItem("city", city)
